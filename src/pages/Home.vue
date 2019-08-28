3046677ddf2af5b52427563d8371ea37cd3b350e
prismistim <prismistim@hotmail.com>
2019-08-29 00:09:12 +0900

    initial commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  .sketch-list
+    Sketch_190828
+</template>
+
+<script>
+import Sketch_190828 from '../sketch/Sketch_190828.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Sketch_190828,
+  },
+}
+</script>
---
+++ src/sketch/Sketch_190828.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .sketch-item
+    #canvas-190828
+</template>
+
+<script>
+  import p5 from 'p5';
+
+  export default {
+    mounted () {
+      const canvas = p5.createCanvas(500, 500)
+      canvas.parent("canvas-190828")
+
+      p5.setup = () => {
+      }
+
+      p5.draw = () => {
+        p5.background(0);
+      }
+    }
+  }
+</script>
